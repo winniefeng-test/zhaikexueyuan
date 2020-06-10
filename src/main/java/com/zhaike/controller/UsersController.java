565a1c4d90511518472010a2c6ce5cb819830364
winniefeng-test <winnie_ent@163.com>
2020-06-10 17:00:44 +0800

    !

--- src/main/java/com/zhaike/controller/UsersController.java
+++ src/main/java/com/zhaike/controller/UsersController.java
@@ -42,9 +42,9 @@ public class UsersController {
     public String validate(String uname, String pwd){
         boolean b = usersService.validateUser(uname, pwd);
         if(b){
-            return "登陆成功";
+            return "登陆成功!";
         }else{
-            return "登录失败";
+            return "登录失败!";
         }
     }
 }
